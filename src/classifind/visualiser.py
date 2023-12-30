27306b3fb4265ac2e7a541e727a5309c0b729ea4
Jia Wei Leong <60588144+JWL16038@users.noreply.github.com>
2023-12-30 19:33:42 +1300

    refactor(visualiser): Replace existing visualiser code with torchaudio tut code

--- src/classifind/visualiser.py
+++ src/classifind/visualiser.py
@@ -1,52 +1,103 @@
 """
 The visualiser used to display the plots of the audio file.
+
+Some code taken from https://pytorch.org/tutorials/beginner/audio_preprocessing_tutorial.html
 """
 import librosa
-import numpy as np
 import torch
 from matplotlib import pyplot as plt
-from torchaudio import transforms
 
 N_FFT = 2048
 
 
-def plot_spectrum(musicdata):
+def plot_waveform(waveform, sample_rate, xlim=None, ylim=None):
     """
-    Visualises the spectrum
+    Visualises the waveform
     """
-    fourier_transform = np.abs(
-        librosa.stft(musicdata.waveform[:N_FFT], hop_length=N_FFT + 1)
-    )
-    plt.figure(figsize=(12, 4))
-    plt.plot(fourier_transform)
-    plt.title("Spectrum")
-    plt.xlabel("Frequency Bin")
-    plt.ylabel("Amplitude")
-    plt.show()
+    waveform = waveform.numpy()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for channel in range(num_channels):
+        axes[channel].plot(time_axis, waveform[channel], linewidth=1)
+        axes[channel].grid(True)
+        if num_channels > 1:
+            axes[channel].set_ylabel(f"Channel {channel+1}")
+        if xlim:
+            axes[channel].set_xlim(xlim)
+        if ylim:
+            axes[channel].set_ylim(ylim)
+    figure.suptitle("Waveform")
+    plt.show(block=False)
 
 
-def plot_waveform_spectogram(musicdata):
+def plot_specgram(waveform, sample_rate, xlim=None):
     """
-    Visualises the waveform and the spectrogram in one plot
+    Visualises the specgram
     """
-    spectrogram = transforms.Spectrogram(n_fft=N_FFT)
-    spec = spectrogram(musicdata.waveform)
-    fig, axes = plt.subplots(2, 1)
-    waveform = musicdata.waveform.numpy()
-    _, num_frames = waveform.shape
-    time_axis = torch.arange(0, num_frames) / musicdata.sample_rate
-    axes[0].plot(time_axis, waveform[0], linewidth=1)
-    axes[0].grid(True)
-    axes[0].set_xlim([0, time_axis[-1]])
-    axes[0].set_xlabel("Time (secs)")
-    axes[0].set_title("Original Waveform")
-    axes[1].imshow(
-        librosa.power_to_db(spec[0]),
-        origin="lower",
-        aspect="auto",
-        interpolation="nearest",
-    )
-    axes[1].set_title("Spectrogram")
-    axes[1].set_ylabel("Frequency Bin")
-    fig.tight_layout()
-    plt.show()
+    waveform = waveform.numpy()
+
+    num_channels, _ = waveform.shape
+    #   time_axis = torch.arange(0, num_frames) / sample_rate
+
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for channel in range(num_channels):
+        axes[channel].specgram(waveform[channel], Fs=sample_rate)
+        if num_channels > 1:
+            axes[channel].set_ylabel(f"Channel {channel+1}")
+        if xlim:
+            axes[channel].set_xlim(xlim)
+    figure.suptitle("Spectrogram")
+    plt.show(block=False)
+
+
+def plot_spectrogram(spec, aspect="auto", xmax=None):
+    """
+    Visualises the spectrogram
+    """
+    fig, axs = plt.subplots(1, 1)
+    axs.set_title("Spectrogram (db)")
+    axs.set_ylabel("Frequency bin")
+    axs.set_xlabel("Frame")
+    image = axs.imshow(librosa.power_to_db(spec), origin="lower", aspect=aspect)
+    if xmax:
+        axs.set_xlim((0, xmax))
+    fig.colorbar(image, ax=axs)
+    plt.show(block=False)
+
+
+def plot_mel_fbank(fbank):
+    """
+    Visualises the Mel F Bank
+    """
+    _, axs = plt.subplots(1, 1)
+    axs.set_title("Filter bank")
+    axs.imshow(fbank, aspect="auto")
+    axs.set_ylabel("Frequency bin")
+    axs.set_xlabel("Mel bin")
+    plt.show(block=False)
+
+
+def plot_pitch(waveform, sample_rate, pitch):
+    """
+    Visualises the pitch
+    """
+    _, axis = plt.subplots(1, 1)
+    axis.set_title("Pitch Feature")
+    axis.grid(True)
+
+    end_time = waveform.shape[1] / sample_rate
+    time_axis = torch.linspace(0, end_time, waveform.shape[1])
+    axis.plot(time_axis, waveform[0], linewidth=1, color="gray", alpha=0.3)
+
+    axis2 = axis.twinx()
+    time_axis = torch.linspace(0, end_time, pitch.shape[1])
+    axis2.plot(time_axis, pitch[0], linewidth=2, label="Pitch", color="green")
+    axis2.legend(loc=0)
+    plt.show(block=False)
